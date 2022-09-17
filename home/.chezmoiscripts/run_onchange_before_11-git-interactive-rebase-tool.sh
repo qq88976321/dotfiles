37f439392fbc9b70177e63556953db171f4e8285
qq88976321 <qq88976321@gmail.com>
2022-09-17 16:22:27 +0800

    feat(script): add script to install interactive-rebase-tool
    Signed-off-by: qq88976321 <qq88976321@gmail.com>

---
+++ home/.chezmoiscripts/run_onchange_before_11-git-interactive-rebase-tool.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Exit on an Error or if an Unset variable is referenced.
+set -eu
+
+# When used in combination with set -e, pipefail will make a script exit if any command in a pipeline errors.
+set -o pipefail
+
+# Print a trace of simple commands and their arguments after they are expanded and before they are executed.
+set -x
+
+if [[ ! -x $(command -v interactive-rebase-tool) ]]; then
+    # ref: https://github.com/MitMaro/git-interactive-rebase-tool/blob/master/readme/install.md
+
+    LATEST_TAG=$(git ls-remote --tags --sort="v:refname" https://github.com/MitMaro/git-interactive-rebase-tool.git '[0-9]*.[0-9]*.[0-9]*' | tail -n1 | sed 's/.*\///; s/\^{}//')
+
+    DIST_ID=$(lsb_release --id --short | awk '{print tolower($0)}')
+    DIST_RELEASE=$(lsb_release --release --short)
+    PKG_NAME=git-interactive-rebase-tool-"$LATEST_TAG"-"$DIST_ID"-"$DIST_RELEASE"_amd64.deb
+
+    curl -fsSL https://github.com/MitMaro/git-interactive-rebase-tool/releases/download/"$LATEST_TAG"/"$PKG_NAME" --output /tmp/"$PKG_NAME"
+    sudo dpkg -i /tmp/git-interactive-rebase-tool-"$LATEST_TAG"-ubuntu-20.04_amd64.deb
+else
+    echo "[INFO] interactive-rebase-tool is already installed."
+fi
