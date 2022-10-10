0c1bd64b4266f94298d218bdfecf3597fff1bb00
qq88976321 <qq88976321@gmail.com>
2022-10-10 23:08:52 +0800

    refactor(script): separate localtime steup
    Signed-off-by: qq88976321 <qq88976321@gmail.com>

--- home/.chezmoiscripts/01-general/run_onchange_before_04-tmux.sh
+++ home/.chezmoiscripts/01-general/run_onchange_before_04-tmux.sh
@@ -13,12 +13,7 @@ if [[ ! -x $(command -v tmux) ]]; then
     # tmux dependencies
     # https://github.com/tmux/tmux/wiki/Installing#from-source-tarball
 
-    # Make sure /etc/localtime exists to prevent pkg-config from asking timezone interactivly.
-    # Run 'dpkg-reconfigure tzdata' if you wish to change it.
-    if [[ ! -f /etc/localtime ]]; then
-        ln -snf /usr/share/zoneinfo/"$(curl https://ipapi.co/timezone)" /etc/localtime
-    fi
-    DEBIAN_FRONTEND=noninteractive sudo apt-get install -yq libevent-dev ncurses-dev build-essential bison pkg-config automake
+    sudo apt-get install -yq libevent-dev ncurses-dev build-essential bison pkg-config automake
 
     # build tmux 3 from source
     LATEST_TAG=$(git ls-remote --tags --sort="v:refname" https://github.com/tmux/tmux.git | tail -n1 | sed 's/.*\///; s/\^{}//')
