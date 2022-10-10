20288fef7ac6f974c590c11c90bea5d972e87c09
qq88976321 <qq88976321@gmail.com>
2022-10-10 18:26:55 +0800

    fix(script): don't hardcode output path
    Signed-off-by: qq88976321 <qq88976321@gmail.com>

--- home/.chezmoiscripts/01-general/run_onchange_before_11-fd-find.sh
+++ home/.chezmoiscripts/01-general/run_onchange_before_11-fd-find.sh
@@ -13,9 +13,10 @@ if [[ ! -x $(command -v fd) ]]; then
     # https://github.com/sharkdp/fd#on-ubuntu
     LATEST_TAG=$(git ls-remote --tags --sort="v:refname" https://github.com/sharkdp/fd.git | tail -n1 | sed 's/.*\///; s/\^{}//')
     LATEST_TAG_WITHOUT_V_PREFIX=$(echo "$LATEST_TAG" | cut -c 2-)
-    curl -fsSL https://github.com/sharkdp/fd/releases/download/"$LATEST_TAG"/fd_"$LATEST_TAG_WITHOUT_V_PREFIX"_amd64.deb --output /tmp/fd_"$LATEST_TAG_WITHOUT_V_PREFIX"_amd64.deb
+    PKG_PATH=/tmp/fd_"$LATEST_TAG_WITHOUT_V_PREFIX"_amd64.deb
+    curl -fsSL https://github.com/sharkdp/fd/releases/download/"$LATEST_TAG"/fd_"$LATEST_TAG_WITHOUT_V_PREFIX"_amd64.deb --output "$PKG_PATH"
 
-    sudo dpkg -i /tmp/fd_8.4.0_amd64.deb
+    sudo dpkg -i "$PKG_PATH"
 else
     echo "[INFO] fd-find is already installed."
 fi
