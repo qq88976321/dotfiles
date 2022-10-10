a6d7f6c7a9933281bcedc1c820f5f4953961dbaa
qq88976321 <qq88976321@gmail.com>
2022-10-10 13:51:36 +0800

    feat(script): install pyenv dependencies
    Signed-off-by: qq88976321 <qq88976321@gmail.com>

--- home/.chezmoiscripts/run_onchange_before_05-pyenv.sh
+++ home/.chezmoiscripts/run_onchange_before_05-pyenv.sh
@@ -13,8 +13,13 @@ set -x
 if [[ ! -x $(command -v pyenv) ]]; then
     LATEST_TAG=$(git ls-remote --tags --sort="v:refname" https://github.com/pyenv/pyenv.git 'v[0-9]*.[0-9]*.[0-9]*' | tail -n1 | sed 's/.*\///; s/\^{}//')
     curl https://pyenv.run | PYENV_GIT_TAG=$LATEST_TAG bash
+
+    # Suggested build environment
+    # https://github.com/pyenv/pyenv/wiki#suggested-build-environment
+    sudo apt-get update
+    sudo apt-get install make build-essential libssl-dev zlib1g-dev \
+        libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+        libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 else
     echo "[INFO] pyenv is already installed."
 fi
-
-
