f978f7348f4fad63fa1bdde78569f8dd4f5286f5
qq88976321 <qq88976321@gmail.com>
2024-02-26 12:30:58 +0800

    fix(script): fix rg download url
    Signed-off-by: qq88976321 <qq88976321@gmail.com>

--- home/.chezmoiscripts/01-general/run_onchange_before_12-ripgrep.sh
+++ home/.chezmoiscripts/01-general/run_onchange_before_12-ripgrep.sh
@@ -13,7 +13,7 @@ if [[ ! -x $(command -v rg) ]]; then
     # only Ubuntu Cosmic (18.10) or newer can use apt-get install to install rg
     LATEST_TAG=$(git ls-remote --tags --sort="v:refname" https://github.com/BurntSushi/ripgrep.git '[0-9]*.[0-9]*.[0-9]*' | tail -n1 | sed 's/.*\///; s/\^{}//')
     PKG_PATH=/tmp/ripgrep_"$LATEST_TAG"_amd64.deb
-    curl -fsSL https://github.com/BurntSushi/ripgrep/releases/download/"$LATEST_TAG"/ripgrep_"$LATEST_TAG"_amd64.deb --output "$PKG_PATH"
+    curl -fsSL https://github.com/BurntSushi/ripgrep/releases/download/"$LATEST_TAG"/ripgrep_"$LATEST_TAG"-1_amd64.deb --output "$PKG_PATH"
 
     sudo dpkg -i "$PKG_PATH"
 else
