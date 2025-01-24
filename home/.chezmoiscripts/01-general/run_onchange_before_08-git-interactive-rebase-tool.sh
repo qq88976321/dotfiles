ed678bda1d543c98df3a9716cd4d46cb079ba7f2
qq88976321 <qq88976321@gmail.com>
2025-01-24 09:19:44 +0800

    fix(script): git-interactive-rebase-tool 2.4.0 requires libc>= 2.34
    - 2.4.0 is too new for ubuntu20.04
    
    Signed-off-by: qq88976321 <qq88976321@gmail.com>

--- home/.chezmoiscripts/01-general/run_onchange_before_08-git-interactive-rebase-tool.sh
+++ home/.chezmoiscripts/01-general/run_onchange_before_08-git-interactive-rebase-tool.sh
@@ -19,11 +19,17 @@ if [[ ! -x $(command -v interactive-rebase-tool) ]]; then
     PKG_NAME=git-interactive-rebase-tool-"$LATEST_TAG"-"$DIST_ID"-"$DIST_RELEASE"_amd64.deb
 
     # FIXME: debian package for ubuntu 22.04 haven't been released, however 20.04 seems work better than latest build?
-    if [[ "$DIST_ID" = "ubuntu" ]] && [[ "$DIST_RELEASE" = "22.04" ]]; then
-        PKG_NAME=git-interactive-rebase-tool-"$LATEST_TAG"-"$DIST_ID"-20.04_amd64.deb
+    if [[ "$DIST_ID" = "ubuntu" ]]; then
+        if [[ "$DIST_RELEASE" = "20.04" ]]; then
+            STABLE_TAG="2.3.0"
+            PKG_NAME=git-interactive-rebase-tool-"$STABLE_TAG"-"$DIST_ID"-20.04_amd64.deb
+        elif [[ "$DIST_RELEASE" = "22.04" ]]; then
+            STABLE_TAG=$LATEST_TAG
+            PKG_NAME=git-interactive-rebase-tool-"$LATEST_TAG"-"$DIST_ID"-20.04_amd64.deb
+        fi
     fi
 
-    curl -fsSL https://github.com/mitmaro/git-interactive-rebase-tool/releases/download/"$LATEST_TAG"/"$PKG_NAME" --output /tmp/"$PKG_NAME"
+    curl -fsSL https://github.com/mitmaro/git-interactive-rebase-tool/releases/download/"$STABLE_TAG"/"$PKG_NAME" --output /tmp/"$PKG_NAME"
     sudo dpkg -i /tmp/"$PKG_NAME"
 
     rm /tmp/"$PKG_NAME"
